9a27874d963c5832ef06b5fec90b83907a8ba4cc
dannateffy99 <127898924+dannateffy99@users.noreply.github.com>
2023-03-17 09:10:03 -0500

    Add files via upload

---
+++ database/migrations/2023_03_16_203345_create_notas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_curso');
+            $table->foreign('id_curso')->references('id')->on('cursos');
+            $table->double('notas3',8,2);
+            $table->double('notas2',8,2);
+            $table->double('notas1',8,2);
+            $table->string('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notas');
+    }
+}
